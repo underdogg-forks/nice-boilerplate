b7187ffeff521b02f2cb52232ee00e0e22ef0850
Haew <hi@haew.xyz>
2020-03-03 21:55:10 +0100

    Add default title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <link href="{{ mix('/build/app.css') }}" rel="stylesheet" />
     <script src="{{ mix('/build/app.js') }}" defer></script>
+    <title>{{ config('app.name', 'Laravel Boilerplate') }}</title>
     {{-- <link rel="stylesheet" href="https://rsms.me/inter/inter.css"> --}}
   </head>
   
