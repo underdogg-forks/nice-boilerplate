ba08379c8bff80a60ea847a5edc49bd8798083b4
Haew <hi@haew.xyz>
2020-03-03 19:53:56 +0100

    Added localization

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,10 +1,12 @@
 import Vue from 'vue';
+import Lang from 'laravel-vue-lang';
 import Composition from '@vue/composition-api';
 import { InertiaApp } from '@inertiajs/inertia-vue';
 
 // @ts-ignore
 Vue.use(InertiaApp);
 Vue.use(Composition);
+Vue.use(Lang);
 
 const app: any = document.getElementById('app');
 
--- resources/js/views/Index.vue
+++ resources/js/views/Index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col items-center justify-center w-screen h-screen text-gray-100 bg-gray-900">
-    <h1 class="text-5xl font-thin">Laravel Boilerplate</h1>
-    <h2>For Laravel {{ version }}</h2>
+    <h1 class="text-5xl font-thin">{{ $t('app.welcome.title') }}</h1>
+    <h2>{{ $_('app.welcome.subtitle', { version }) }}</h2>
   </div>
 </template>
 
